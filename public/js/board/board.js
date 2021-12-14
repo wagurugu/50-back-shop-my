77b4962518d28733094232909942f73255db7c35
wagurugu <cutygost@nate.com>
2021-12-14 11:49:39 +0900

    ..

--- public/js/board/board.js
+++
@@ -1,5 +0,0 @@
-$('.board-container.list-page.gallery-type .list').click(onBoardListClick)
-function onBoardListClick() {
-  console.log(this)
-  location.href = $(this).data('link') + '?boardType=gallery'
-}
--- public/js/board/gallery-list.js
+++
@@ -1,4 +0,0 @@
-$('.gallery-type.list-page .list').click(onGalleryClick);
-function onGalleryClick() {
-  location.href = $(this).data('link');
-}
